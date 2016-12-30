b70879d6cd6f4b82ab3c4d0b05bc18288df344bd
zucel <zucel_9@hotmail.com>
2016-12-29 19:19:25 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace vegetavideoju
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.vegetaLeft = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p6 = new System.Windows.Forms.PictureBox();
+            this.p5 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.p7 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.vegetaLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vegetaLeft
+            // 
+            this.vegetaLeft.BackColor = System.Drawing.Color.Transparent;
+            this.vegetaLeft.Image = global::vegetavideoju.Properties.Resources.vegeta_agarrando;
+            this.vegetaLeft.Location = new System.Drawing.Point(140, 206);
+            this.vegetaLeft.Name = "vegetaLeft";
+            this.vegetaLeft.Size = new System.Drawing.Size(100, 151);
+            this.vegetaLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.vegetaLeft.TabIndex = 5;
+            this.vegetaLeft.TabStop = false;
+            this.vegetaLeft.Click += new System.EventHandler(this.vegetaLeft_Click);
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.Image = global::vegetavideoju.Properties.Resources._1;
+            this.p1.Location = new System.Drawing.Point(2, 1);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(100, 73);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1.TabIndex = 7;
+            this.p1.TabStop = false;
+            this.p1.Click += new System.EventHandler(this.p1_Click);
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.Image = global::vegetavideoju.Properties.Resources._2;
+            this.p2.Location = new System.Drawing.Point(488, 56);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(100, 50);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2.TabIndex = 8;
+            this.p2.TabStop = false;
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.Transparent;
+            this.p3.Image = global::vegetavideoju.Properties.Resources._3;
+            this.p3.Location = new System.Drawing.Point(12, 290);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(100, 50);
+            this.p3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p3.TabIndex = 9;
+            this.p3.TabStop = false;
+            this.p3.Click += new System.EventHandler(this.p3_Click);
+            // 
+            // p6
+            // 
+            this.p6.BackColor = System.Drawing.Color.Transparent;
+            this.p6.Image = global::vegetavideoju.Properties.Resources._6;
+            this.p6.Location = new System.Drawing.Point(310, 290);
+            this.p6.Name = "p6";
+            this.p6.Size = new System.Drawing.Size(100, 50);
+            this.p6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p6.TabIndex = 10;
+            this.p6.TabStop = false;
+            // 
+            // p5
+            // 
+            this.p5.BackColor = System.Drawing.Color.Transparent;
+            this.p5.Image = global::vegetavideoju.Properties.Resources.esfera_de_5_estrellas;
+            this.p5.Location = new System.Drawing.Point(549, 355);
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(67, 45);
+            this.p5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p5.TabIndex = 11;
+            this.p5.TabStop = false;
+            // 
+            // p4
+            // 
+            this.p4.BackColor = System.Drawing.Color.Transparent;
+            this.p4.Image = global::vegetavideoju.Properties.Resources._4;
+            this.p4.Location = new System.Drawing.Point(151, 24);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(100, 50);
+            this.p4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p4.TabIndex = 12;
+            this.p4.TabStop = false;
+            // 
+            // p7
+            // 
+            this.p7.BackColor = System.Drawing.Color.Transparent;
+            this.p7.Image = global::vegetavideoju.Properties.Resources.esfera_de_7_estrellas;
+            this.p7.Location = new System.Drawing.Point(287, 96);
+            this.p7.Name = "p7";
+            this.p7.Size = new System.Drawing.Size(67, 45);
+            this.p7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p7.TabIndex = 13;
+            this.p7.TabStop = false;
+            this.p7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::vegetavideoju.Properties.Resources.paisaje700;
+            this.ClientSize = new System.Drawing.Size(701, 485);
+            this.Controls.Add(this.p7);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p5);
+            this.Controls.Add(this.p6);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.vegetaLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.vegetaLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.PictureBox vegetaLeft;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p6;
+        private System.Windows.Forms.PictureBox p5;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.PictureBox p7;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vegetavideoju
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (p1.Bounds.IntersectsWith(vegetaLeft.Bounds))
+            {
+                p1.Visible = false;
+            } if (p2.Bounds.IntersectsWith(vegetaLeft.Bounds))
+            {
+                p2.Visible = false;
+            }
+            if (p3.Bounds.IntersectsWith(vegetaLeft.Bounds))
+            {
+                p3.Visible = false;
+            }
+            if (p4.Bounds.IntersectsWith(vegetaLeft.Bounds))
+            {
+                p4.Visible = false;
+            }
+            if (p5.Bounds.IntersectsWith(vegetaLeft.Bounds))
+            {
+                p5.Visible = false;
+            }
+            if (p6.Bounds.IntersectsWith(vegetaLeft.Bounds))
+            {
+                p6.Visible = false;
+            }
+            if (p7.Bounds.IntersectsWith(vegetaLeft.Bounds))
+            {
+                p7.Visible = false;
+            }
+
+
+             switch (e.KeyData)
+              {
+                
+                  case Keys.Left:
+                 
+                     vegetaLeft.Left -= 50;  //mueve la image 
+                      break;
+
+
+
+                  case Keys.Right:
+                     
+                      vegetaLeft.Left += 60;
+                      break;
+
+
+
+                 case Keys.Up:
+                      
+                      vegetaLeft.Top -= 60;                      
+                      break;
+
+
+
+                 case Keys.Down:
+                      
+                     vegetaLeft.Top += 60;
+                      break;
+            
+              }
+        }
+
+        private void vegetaLeft_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vegetaRigth_Click(object sender, EventArgs e)
+        {
+         
+         
+                  
+
+
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x = 0;
+            do
+            {
+                p1.Top = p1.Top + 10;
+                p2.Top = p1.Top + 10;
+                p3.Top = p1.Top + 10;
+                p4.Top = p1.Top + 10;
+                p5.Top = p1.Top + 10;
+                p6.Top = p1.Top + 10;
+                p7.Top = p1.Top + 10;   
+            } while (x > 1);
+
+
+
+        }
+
+        private void p3_Click(object sender, EventArgs e)
+        {
+
+        }
+        }
+    }
+
